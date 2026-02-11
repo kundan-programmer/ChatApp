066626d00ffbdc3701233500bcac3d4b20b67daf
kundan-programmer <kk5471823@gmail.com>
2026-02-11 23:02:44 +0530

    Added axios baseURL env config

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,6 +6,11 @@ import App from './App.jsx';
 import { AuthProvider } from "./context/Authprovider.jsx";
 import { BrowserRouter } from 'react-router-dom';
 import { SocketProvider } from './context/SocketContext.jsx';
+import axios from 'axios';
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+
+
 createRoot(document.getElementById('root')).render(
    <BrowserRouter>
     <AuthProvider>
